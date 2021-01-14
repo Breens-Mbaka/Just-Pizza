430330484b90e612a863b64af5dce29841868e30
Breens-Thomas <mbakabreens@gmail.com>
2021-01-14 06:06:27 +0300

    Add objects to store data for each item(price and name

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+//Business logic
+//Store data for the name of each item
+var hawaiian = {
+    name:"hawaiian",
+    price: 19.99
+}
+var borewors = {
+    name:"borewors",
+    price: 19.00
+}
+var bbqSteak = {
+    name:"BBQ Steak",
+    price: 21.00
+}
+var chickenTikka = {
+    name:"Chicken Tikka",
+    price: 20.00
+}
+var chickenBacon = {
+    name:"Chicken Bacon BBQ",
+    price: 19.55
+}
+var vegFeast = {
+    name:"Veg Feast",
+    price: 22.00
+}
+var spicyBorewors = {
+    name:"Spicy Borewors",
+    price: 22.50
+}
+
+var chickenPepperoni = {
+    name:"Chicken Pepperoni",
+    price: 23.00
+}
+var chickenMushroom = {
+    name: "Chicken Mushroom",
+    price: 24.00
+}
+var nyamaFeast = {
+    name: "Nyama Feast",
+    price: 24.55
+}
+var roastVeg = {
+    name: "Roast Veg & Feta",
+    price: 25.00
+}
+var periChicken = {
+    name: "Peri Peri Chicken",
+    price: 26.00
+}
