eac9dc6e334bce0ffacd11db87c3b2496d5f3e18
Breens-Thomas <mbakabreens@gmail.com>
2021-01-14 20:08:39 +0300

    Add animate scroll also make the pickup option to exclude delivery fees

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 //User interface logic
-(document).ready(function(event) {
-    event.preventDefault();
+$(document).ready(function() {
 
     //user interface logic
     $("#order-1").click(function() {
@@ -8,7 +7,6 @@
         var crust = $(".crust input[type='radio']:checked").val();
         var toppings = $(".toppings input[type='radio']:checked").val();
         var number = $("#quantity").val();
-        var delivery = $(".delivery input[type='radio']:checked").val();
         var location = $("#input-location").val();
         var deliveryFee = 1.99
 
@@ -17,7 +15,7 @@
             text: `Number of Pizzas: ${number}`,
             text: `Delivery fee: ${deliveryFee}`,
             text: `Delivery address${location}`,
-            text: `Order summary: You bought ${number} ${size} pizzas with ${toppings} and a ${crust} crust Your price is ${totalSum()}.It will be delivered in ${location}`,
+            text: `Order summary: You bought ${number} ${size} pizzas with ${toppings} and a ${crust} crust Your price is $ ${totalSum()}.It will be delivered in ${location}`,
             icon: "success",
             buttons: true,
         });
@@ -30,6 +28,25 @@
         })
         .change();
 
+        $("#button-2,#button-3,#button-4,#button-5,#button-6,#button-7,#button-8,#button-9,#button-10,#button-11,#button-12")
+        .click(function() {
+            location.href = "order.html";
+        })
+        $("#button-1").click(function(){
+
+        })
+
+        $(document).scroll(function () {
+            var $nav = $(".navbar");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+          });
+
+          $("#button-1").click(function() {
+            $('html,body').animate({
+                scrollTop: $(".classic-pizza").offset().top},
+                'slow');
+        });
+
 
     //Business Interface logic
     var pizza = {
@@ -40,27 +57,33 @@
         crust: 2
     }
 
-    var deliveryFee = 1.99
-
+    var deliveryFee = 1.99;
+    var delivery = $(".delivery input[type='radio']:checked").val();
     function totalSum() {
         var size = $(".size input[type='radio']:checked").val();
         var number = $("#quantity").val();
-        console.log(number)
-        if (size === "small") {
-            var smallPizza = pizza.smallPrice
-            var toppings = pizza.topping
-            var crusts = pizza.crust
-            return ((smallPizza.SM * number) + toppings + crusts);
-        } else if (size === "medium") {
+        if(delivery === "pickup") {
             var mediumPizza = pizza.mediumPrice
             var toppings = pizza.topping
             var crusts = pizza.crust
             return ((mediumPizza * number) + toppings + crusts);
-        } else {
+        }
+        else if (size === "small") {
+            var smallPizza = pizza.smallPrice
+            var toppings = pizza.topping
+            var crusts = pizza.crust
+            return ((smallPizza * number) + toppings + crusts + deliveryFee);
+        } else if (size === "medium") {
+            var mediumPizza = pizza.mediumPrice
+            var toppings = pizza.topping
+            var crusts = pizza.crust
+            return ((mediumPizza * number) + toppings + crusts + deliveryFee);
+        } 
+        else {
             var largePizza = pizza.largePrice
             var toppings = pizza.topping
             var crusts = pizza.crust
-            return ((pizza * number) + toppings + crusts);
+            return ((largePizza * number) + toppings + crusts + deliveryFee);
         }
     }
 })
